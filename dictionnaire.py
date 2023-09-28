c5fb525bfee1e2f96457b5d5294be6f70ec4aea8
Maxime Barthomeuf <maxime.barthomeuf@gmail.com>
2023-09-28 10:02:16 +0200

    decoupe phrase

---
+++ dictionnaire.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+ * @author : maxime
+ * @email : maxime.barthomeuf@cpe.fr
+ * @date : 28/09/2023, jeudi
+ * TODO :
+"""
+
+d = {
+    "le": 0,
+    "la": 0,
+    "les": 0,
+    "un": 0,
+    "une": 0,
+    "des": 0,
+    "beau": 1,
+    "joli": 1,
+    "grand": 1,
+    "petit": 1,
+    "gros": 1,
+    "vieux": 1,
+    "nouveau": 1,
+    "jeux": 2,
+    "livre": 2,
+    "stylo": 2,
+    "ordinateur": 2,
+    "ananas": 2,
+    "pomme": 2,
+    "poire": 2,
+    "mange": 3,
+    "vend": 3,
+    "achète": 3,
+    "donne": 3,
+    "est": 3,
+    "suis": 3,
+    "sont": 3,
+    "olivier": 4,
+    "maxime": 4,
+    "jean": 4,
+    "pierre": 4,
+    "paul": 4,
+    "marie": 4,
+    ".": 5,
+    "?": 5,
+    "!": 5,
+    ",": 5,
+    ";": 5,
+    ":": 5
+}
---
+++ main.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+"""
+ * @author : maxime et olivier
+ * @email : maxime.barthomeuf@cpe.fr
+ * @date : 28/09/2023, jeudi
+ * TODO :
+"""
+
+import dictionnaire
+
+dictionnaire = dictionnaire.d
+
+tableau_transitions = (
+    (1, 8, 8, 8, 4, 8),
+    (8, 1, 2, 8, 8, 8),
+    (8, 2, 8, 3, 8, 8),
+    (5, 8, 8, 8, 7, 9),
+    (8, 8, 8, 3, 8, 8),
+    (8, 5, 6, 8, 8, 0),
+    (8, 6, 8, 8, 8, 9),
+    (8, 8, 8, 8, 8, 9),
+    (8, 8, 8, 8, 8, 8),
+    (1, 8, 8, 8, 4, 8)
+)
+
+phrase = "Olivier vend un ananas."
+
+
+def decoupage_liste(string):
+    # decoupe la phrase en liste de mots et de ponctuation
+    liste = []
+    mot = ""
+
+    ponctuations = [a for a in dictionnaire if dictionnaire[a] == 5]
+
+    string = string.lower()
+
+    if string[-1] == " ":
+        string = string[:-1]
+
+    for i in string:
+        if i == " ":
+            liste.append(mot)
+            mot = ""
+        elif i in ponctuations:
+            liste.append(mot)
+            liste.append(i)
+            mot = ""
+        else:
+            mot += i
+
+    liste.append(mot)
+
+    return liste
+
+
+print(decoupage_liste(phrase))
